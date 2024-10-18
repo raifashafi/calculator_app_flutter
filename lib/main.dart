a34fb19ba1cdfb32c5f464fe4fef5bbdcdb61d94
raifashafi <raifafisat@gmail.com>
2024-10-18 15:07:01 +0530

    created

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+void main()
+{
+  runApp();
+}
+
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
